15e25e8130a7776a826332c1d7297eb73d173cf4
dais-k <dqm11314@nifty.com>
2025-07-06 19:19:05 +0900

    全翼型戦闘爆撃機に対応

--- main/logbook/internal/ItemIconType.java
+++ main/logbook/internal/ItemIconType.java
@@ -72,6 +72,7 @@ public class ItemIconType {
             this.put(56, "噴式局地戦闘機");
             this.put(57, "試作局地戦闘機");
             this.put(58, "夜間爆撃機");
+            this.put(59, "全翼戦闘爆撃機");
         }
     };
 
